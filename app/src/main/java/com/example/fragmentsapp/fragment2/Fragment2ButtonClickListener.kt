ac478028f0e404144d7112c22dd0c7062d1157a1
dobrov.v <dobrov.v@promail366.com>
2023-08-19 14:40:35 +0300

    Num 3 работа с фрагментами, отображение навигация между фрагментами, передача данных между фрагментами с помощью fragmentResultApi

--- app/src/main/java/com/example/fragmentsapp/fragment2/Fragment2ButtonClickListener.kt
+++ app/src/main/java/com/example/fragmentsapp/fragment2/Fragment2ButtonClickListener.kt
@@ -1,7 +1,5 @@
 package com.example.fragmentsapp.fragment2
 
 internal interface Fragment2ButtonClickListener {
-    fun backToFragment1()
-    fun openFragment1Again()
-    fun openFragment2AgainWithNewText(newText: String)
+    fun backToFragment1WithChanges()
 }
